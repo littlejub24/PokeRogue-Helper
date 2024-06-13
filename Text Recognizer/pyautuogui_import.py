8b7d5489617a6d8a4025c7710dc9c2387e2004f0
littlejub <zachhuamani@gmail.com>
2024-06-13 18:27:31 -0400

    test

--- Text Recognizer/pyautuogui_import.py
+++ Text Recognizer/pyautuogui_import.py
@@ -3,7 +3,8 @@ import pyautogui
 print("hi")
 
 try: 
-    res = pyautogui.locateOnScreen('edit.png')
+    res = pyautogui.locateOnScreen(r"C:\Users\Zach\Desktop\PokeRouge\PokeRogue-Helper\Text Recognizer\edit.png")
     print(res)
 except pyautogui.ImageNotFoundException:
     print("image not found on screen")
+    
