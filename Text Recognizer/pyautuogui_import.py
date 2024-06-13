223e0a2fb84e1e38a60001999931c7b509f71c26
littlejub <zachhuamani@gmail.com>
2024-06-13 18:31:06 -0400

    move images

--- Text Recognizer/pyautuogui_import.py
+++ Text Recognizer/pyautuogui_import.py
@@ -1,9 +1,7 @@
 import pyautogui
 
-print("hi")
-
 try: 
-    res = pyautogui.locateOnScreen(r"C:\Users\Zach\Desktop\PokeRouge\PokeRogue-Helper\Text Recognizer\edit.png")
+    res = pyautogui.locateOnScreen(r"C:\Users\Zach\Desktop\PokeRouge\PokeRogue-Helper\Text Recognizer\Images\edit.png")
     print(res)
 except pyautogui.ImageNotFoundException:
     print("image not found on screen")
