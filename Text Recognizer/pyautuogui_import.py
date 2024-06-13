2ad5ac678edb333c5ed1353d89a76deddadf1d0d
littlejub <zachhuamani@gmail.com>
2024-06-13 18:25:36 -0400

    testing hi

--- Text Recognizer/pyautuogui_import.py
+++ Text Recognizer/pyautuogui_import.py
@@ -3,7 +3,7 @@ import pyautogui
 print("hi")
 
 try: 
-    res = pyautogui.locateOnScreen("edit.png")
+    res = pyautogui.locateOnScreen('edit.png')
     print(res)
 except pyautogui.ImageNotFoundException:
     print("image not found on screen")
