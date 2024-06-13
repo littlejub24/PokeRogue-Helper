347f1cf6de08f7fcbb645d24036b96f240b726e6
littlejub <zachhuamani@gmail.com>
2024-06-13 18:32:26 -0400

    add grass finder

--- Text Recognizer/pyautuogui_import.py
+++ Text Recognizer/pyautuogui_import.py
@@ -3,6 +3,9 @@ import pyautogui
 try: 
     res = pyautogui.locateOnScreen(r"C:\Users\Zach\Desktop\PokeRouge\PokeRogue-Helper\Text Recognizer\Images\edit.png")
     print(res)
+
+    grass = pyautogui.locateOnScreen(r"C:\Users\Zach\Desktop\PokeRouge\PokeRogue-Helper\Text Recognizer\Images\Grass.png")
+    print (grass)
 except pyautogui.ImageNotFoundException:
     print("image not found on screen")
     
